7a130693fb2e11bdc8c01f0cfccff7c8e1a2dbfe
Hsieh11 <Chrishsieh@live.com>
2017-04-05 19:19:30 -0400

    url change

--- server/src/config/constants/constants.ts
+++ server/src/config/constants/constants.ts
@@ -3,7 +3,7 @@
  */
 
 class Constants {
-    static DB_CONNECTION_STRING: string = process.env.NODE_ENV === 'production' ? process.env.dbURI : "mongodb://user:pass@ds149030.mlab.com:49030/starter-db"
+    static DB_CONNECTION_STRING: string = "mongodb://user:pass@ds149030.mlab.com:49030/starter-db"
 }
 Object.seal(Constants);
 export = Constants;
