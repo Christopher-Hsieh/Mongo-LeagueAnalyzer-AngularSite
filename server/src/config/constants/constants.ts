88fac1757caa9e2bf8a085a57847fed2222248cd
Hsieh11 <Chrishsieh@live.com>
2017-04-06 20:39:56 -0400

    fixed typo

--- server/src/config/constants/constants.ts
+++ server/src/config/constants/constants.ts
@@ -6,7 +6,6 @@ class Constants {
     // static DB_CONNECTION_STRING: string = "mongodb://user:pass@ds149030.mlab.com:49030/starter-db"
      static DB_CONNECTION_STRING: string = process.env.NODE_ENV === 'production' ? process.env.dbURI : "mongodb://localhost:27017/quickStart"
 
-    }
 }
 Object.seal(Constants);
 export = Constants;
